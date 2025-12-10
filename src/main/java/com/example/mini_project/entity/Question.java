31c28bbf5654c26a90cb5c0316903519d124809b
hyuuuun <pam4522@gmail.com>
2025-12-10 16:09:41 +0900

    feat : question,tag entity 생성

--- src/main/java/com/example/mini_project/entity/Question.java
+++ src/main/java/com/example/mini_project/entity/Question.java
@@ -1,4 +1,30 @@
 package com.example.mini_project.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    private String created_at;
+
 }
--- src/main/java/com/example/mini_project/entity/Tag.java
+++ src/main/java/com/example/mini_project/entity/Tag.java
@@ -1,4 +1,29 @@
 package com.example.mini_project.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "tag")
+    private List<Question> questions = new ArrayList<>();
+
+
 }

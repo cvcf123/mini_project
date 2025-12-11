1d525edcb441202366e4640437fc9ff5ac01cea3
hyuuuun <pam4522@gmail.com>
2025-12-11 15:52:15 +0900

    fix : AnswerServiceImpl

--- src/main/java/com/example/mini_project/repository/AnswerRepository.java
+++ src/main/java/com/example/mini_project/repository/AnswerRepository.java
@@ -1,9 +1,13 @@
 package com.example.mini_project.repository;
 
 import com.example.mini_project.entity.Answer;
+import com.example.mini_project.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionOrderByCreatedAtAsc(Question question);
 }
--- src/main/java/com/example/mini_project/service/AnswerServiceImpl.java
+++ src/main/java/com/example/mini_project/service/AnswerServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.mini_project.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.mini_project.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
 import com.example.mini_project.dto.AnswerResponseDto;
@@ -19,33 +20,28 @@ public class AnswerServiceImpl implements AnswerService {
 
     private final AnswerRepository answerRepository;
 
+     private final QuestionRepository questionRepository;
+
     @Override
     public List<AnswerResponseDto> getAnswersByQuestion(Long questionId) {
-        return null;
-    }
-    // private final QuestionRepository questionRepository;
 
-   // @Override
-   // public List<AnswerResponseDto> getAnswersByQuestion(Long questionId) {
-
-        /*// 질문 조회
+        // 질문 조회
         Question question = questionRepository.findById(questionId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 질문이 존재하지 않습니다."));*/
-    	
+                .orElseThrow(() -> new IllegalArgumentException("해당 질문이 존재하지 않습니다."));
+
 
         // 해당 질문에 달린 답변 조회
-       // List<Answer> answers = answerRepository.findByQuestionOrderByCreatedAtAsc(question);
-/*
+        List<Answer> answers = answerRepository.findByQuestionOrderByCreatedAtAsc(question);
+
         // DTO 변환
         return answers.stream()
                 .map(answer -> AnswerResponseDto.builder()
                         .id(answer.getId())
                         .content(answer.getContent())
                         .userName(answer.getUser().getName())
-                        .created_at(answer.getCreated_at())
+                        .createdAt(answer.getCreatedAt())
                         .build()
                 ).collect(Collectors.toList());
-    }*/
-    	
-	}
-//}
+    }
+
+}

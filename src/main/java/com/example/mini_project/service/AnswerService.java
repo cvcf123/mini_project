8bbb04626e681dbb4f1c44e4b6f454515c33f4ab
hyuuuun <pam4522@gmail.com>
2025-12-11 14:53:00 +0900

    fix : answerSErive getAnserByQusetion 생성

--- src/main/java/com/example/mini_project/service/AnswerService.java
+++ src/main/java/com/example/mini_project/service/AnswerService.java
@@ -1,5 +1,10 @@
 package com.example.mini_project.service;
 
+import com.example.mini_project.dto.AnswerResponseDto;
+
+import java.util.List;
+
 public interface AnswerService {
 
+    List<AnswerResponseDto> getAnswersByQuestion(Long questionId);
 }

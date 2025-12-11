9131345f78ce5c217769565758212e9a47fe5790
hyuuuun <pam4522@gmail.com>
2025-12-11 16:24:32 +0900

    test : QuestionConrollerTest.java 테스트

--- src/test/java/com/example/mini_project/controller/QuestionConrollerTest.java
+++ src/test/java/com/example/mini_project/controller/QuestionConrollerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,7 +40,7 @@ class QuestionControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private QuestionService questionService;
 
     private MyUserDetails userDetails;

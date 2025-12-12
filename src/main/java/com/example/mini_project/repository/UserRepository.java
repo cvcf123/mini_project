b7d885d8ee1b9a8ba9860c436adab0361b125caa
hyuuuun <pam4522@gmail.com>
2025-12-12 14:41:52 +0900

    fix: CommentControllerTest SecurityMockMvcRequestPostProcessors 인증 후 테스트

--- src/main/java/com/example/mini_project/repository/UserRepository.java
+++ src/main/java/com/example/mini_project/repository/UserRepository.java
@@ -4,14 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.mini_project.entity.Answer;
-import com.example.mini_project.entity.Comment;
 import com.example.mini_project.entity.User;
 
 public interface UserRepository extends JpaRepository <User,Long> {
     boolean existsByEmail(String email);
-    public interface AnswerRepository extends JpaRepository<Answer, Long> {}
-    public interface CommentRepository extends JpaRepository<Comment, Long> {}
-    Optional<User> findByUser(String user);
     Optional<User> findByEmail(String email);
 }
--- src/test/java/com/example/mini_project/controller/CommentControllerTest.java
+++ src/test/java/com/example/mini_project/controller/CommentControllerTest.java
@@ -2,18 +2,27 @@ package com.example.mini_project.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.example.mini_project.config.MyUserDetails;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +39,21 @@ public class CommentControllerTest {
     @MockitoBean
     private CommentService commentService;
 
-    @Test
+    private MyUserDetails userDetails;
+
+    @BeforeEach
+    void setUp() {
+        userDetails = MyUserDetails.builder()
+                .id(1L)
+                .username("test@test.com")
+                .password("password")
+                .name("테스트유저")
+                .email("test@test.com")
+                .authorities(Collections.emptyList())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+        @Test
     void createComment_success() throws Exception {
         CommentResponseDto response = CommentResponseDto.builder()
                 .id(1L)
@@ -51,6 +74,8 @@ public class CommentControllerTest {
 
         mockMvc.perform(
         	    post("/comments")
+                        .with(SecurityMockMvcRequestPostProcessors.user(userDetails))
+                        .with(csrf())
         	        .contentType(org.springframework.http.MediaType.APPLICATION_JSON) // 패키지 명시
         	        .content(jsonRequest)
         	)
@@ -81,7 +106,10 @@ public class CommentControllerTest {
 
         when(commentService.getComments(1L)).thenReturn(list);
 
-        mockMvc.perform(get("/comments/answer/1"))
+        mockMvc.perform(get("/comments/answer/1")
+                        .with(SecurityMockMvcRequestPostProcessors.user(userDetails))
+                )
+
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(1L))
                 .andExpect(jsonPath("$[0].content").value("댓글 내용1"))
